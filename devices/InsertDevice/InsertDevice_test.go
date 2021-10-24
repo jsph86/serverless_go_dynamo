481c7aac8aa0945d850ac1dd34217faf2b533c5d
gholivand.y@gmail.com <gholivand.y@gmail.com>
2021-10-24 17:29:41 +0330

    just another code review.

--- devices/InsertDevice/InsertDevice_test.go
+++ devices/InsertDevice/InsertDevice_test.go
@@ -36,8 +36,6 @@ func TestHandler(t *testing.T) {
 			Body:       `{ "id": "Tobi" }`,
 			Headers: map[string]string{
 				"Content-Type": "application/json",
-				"X-Foo":        "bar",
-				"Host":         "example.com",
 			},
 		}
 
@@ -47,7 +45,7 @@ func TestHandler(t *testing.T) {
 		}
 	})
 
-	t.Run("Insert New Item Without ID attribute", func(t *testing.T) {
+	t.Run("Insert New Item Without ID Attribute", func(t *testing.T) {
 		m := mockedPutItem{
 			Response: dynamodb.PutItemOutput{},
 		}
@@ -60,17 +58,15 @@ func TestHandler(t *testing.T) {
 		e := events.APIGatewayProxyRequest{
 			HTTPMethod: "POST",
 			Path:       "/insertdevice",
-			Body:       `{ "id": "Tobi" }`,
+			Body:       `{ "idd": "Tobi" }`,
 			Headers: map[string]string{
 				"Content-Type": "application/json",
-				"X-Foo":        "bar",
-				"Host":         "example.com",
 			},
 		}
 
-		_, err := d.Handler(e)
-		if err != nil {
-			t.Fatal(err)
+		res, _ := d.Handler(e)
+		if res.StatusCode != 201 {
+			t.Fatal("Test Failed : " + res.Body)
 		}
 	})
 
